2ec5cc86e48e74c9d716233782bef880182ab7b4
Juan E Kipes <juanekipes@gmail.com>
2018-10-10 15:54:58 -0300

    commit inicial

---
+++ src/test/java/ar/com/jekipes/AppTest.java
@@ -0,0 +1,15 @@
+package ar.com.jekipes;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class AppTest {
+  private static final Logger logger = LoggerFactory.getLogger(AppTest.class.getSimpleName());
+  @Test
+  public void evalTest() {
+	logger.info("hello test!!!");
+        assertTrue( true ); 
+  }
+}
